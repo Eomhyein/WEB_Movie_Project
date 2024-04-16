35fee33402638b8ce9a1b7faff28f41fc982d7ae
엄혜인 <mickey_8688@naver.com>
2024-04-16 11:24:45 +0900

    third commit
    영화 기록하기 버튼 누르면 기록하는 카드 열고 닫히게 하기,
    기록하기 버튼 누르면 영화 리뷰 저장하기
    Firebase 연결해서 영화 리뷰 작성한 것 보여주기

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-function openclose() {
-  $('#postingbox').toggle();
-}
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
+//Firebase 내꺼 주소 노출 안되게 올리기
+const firebaseConfig = {
 
-function makeCard() {
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//id 값에 click을 준 것!
+$("#postingbtn").click(async function () {
   let image = $('#image').val();
-  let title = $('#title').val();
-  let comment = $('#comment').val();
+  let titIe = $('#titIe').val();
   let star = $('#star').val();
+  let comment = $('#comment').val();
+
+  let doc = {
+    'image': image,
+    'titIe': titIe,
+    'star': star,
+    'comment': comment
+  };
+  await addDoc(collection(db, "movies"), doc);
+  alert('저장 완료');
+  window.location.reload();
+})
+
+$("#savebtn").click(async function () {
+  $('#postingbox').toggle();
+})
+
+//카드 붙이는 코드
+let docs = await getDocs(collection(db, "movies"));
+docs.forEach((doc) => {
+  let row = doc.data();
+
+  let image = row['image'];
+  let title = row['title'];
+  let comment = row['comment'];
+  let star = row['star'];
 
   let temp_html = `
   <div class="col">
@@ -23,4 +59,4 @@ function makeCard() {
   </div>`;
 
   $('#card').append(temp_html);
-}
+});
